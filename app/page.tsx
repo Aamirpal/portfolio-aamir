8bcc64a24d87f85fae13fb032da781f0b46c695d
Aamirpal Sehmi <aamirpal.sehmi@gmail.com>
2024-08-25 21:27:04 -0600

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 
 import { siteConfig } from "@/config/site";
@@ -9,16 +7,13 @@ import { GithubIcon } from "@/components/icons";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        {/* Building scalable solutions with passion and precision. */}
+    <section className="min-h-screen flex flex-col items-center justify-center gap-4 py-8 md:py-10 -mt-24">
+      <div className="inline-block max-w-xl text-center">
         <h1 className={title()}>Building&nbsp;</h1>
         <h1 className={title({ color: "blue" })}>scalable&nbsp;</h1>
         <br />
         <h1 className={title()}>solutions with passion and precision.</h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          {`Let's build something amazing together.`}
-        </h2>
+        <h2 className="mt-4">{`Let's build something amazing together.`}</h2>
       </div>
 
       <div className="flex gap-3">
